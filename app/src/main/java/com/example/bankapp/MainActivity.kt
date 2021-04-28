fe55e8bba6a262a11f416eeef8a446f0ed3ac3fa
João <jpsdb10@hotmail.com>
2021-04-28 08:50:10 -0300

    Projeto de aplicativo de banco desenvolvido na plataforma DIO

---
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.bankapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvList: RecyclerView
+    private var adapter = MenuItemAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iniciaViews()
+        setItensList()
+    }
+
+    private fun iniciaViews() {
+        rvList = findViewById(R.id.rv_list)
+        rvList.adapter = adapter
+        rvList.layoutManager = GridLayoutManager(this, 2)
+    }
+
+    private fun setItensList() {
+        adapter.setItensList(
+                arrayListOf(
+                        MenuItemModel(
+                                "cartões"
+                        ),
+                        MenuItemModel(
+                                "meus comprovantes"
+                        ),
+                        MenuItemModel(
+                                "investimentos"
+                        ),
+                        MenuItemModel(
+                                "portabilidade de salário"
+                        ),
+                        MenuItemModel(
+                                "crédito"
+                        ),
+                        MenuItemModel(
+                                "seguro"
+                        ),
+                        MenuItemModel(
+                                "cartões"
+                        ),
+                        MenuItemModel(
+                                "meus comprovantes"
+                        ),
+                        MenuItemModel(
+                                "investimentos"
+                        ),
+                        MenuItemModel(
+                                "portabilidade de salário"
+                        ),
+                        MenuItemModel(
+                                "crédito"
+                        ),
+                        MenuItemModel(
+                                "seguro"
+                        )
+                )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/bankapp/MenuItemModel.kt
@@ -0,0 +1,5 @@
+package com.example.bankapp
+
+class MenuItemModel(
+        val titulo: String
+)
